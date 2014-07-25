7797c02f072a74b7d9db408f2e0d159205d2d333
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2014-07-25 10:17:04 -0400

    high pixel density support
    working on adding high pixel density support

--- JinxEngine.js
+++ JinxEngine.js
@@ -158,9 +158,34 @@ function GameEngine(){
 		
 	};
 	
+	this.getDisplayPixelDensity = function(canvas,context){
+		var ratio = 1;
+		if(context.webkitBackingStorePixelRatio < 2){
+			// default to 1 if property not set //
+			ratio = window.devicePixelRatio || 1;
+		};
+		// resize canvas' logical size (ensure CSS maintains original size)//
+		var w = context.canvas.width;
+		var h = context.canvas.height;
+		canvas.attr('width', w*ratio).attr('height', h*ratio);
+  		context.save();
+		context.scale(ratio, ratio);
+		context.beginPath();
+		context.rect(10,10,100,20);
+		context.fillStyle = '#CC0000';
+		context.strokeStyle = '#000000';
+		context.fill();
+		context.stroke();
+		context.restore();
+  		
+		//return window.devicePixelRatio;
+	}
+	
 	this.setDisplay = function(canvas){
 		this.displayDomId = document.getElementById(canvas);
 		this.display = document.getElementById(canvas).getContext(this.context);
+		this.getDisplayPixelDensity(canvas,this.display);
+
 	}
 	
 	this.addObject = function(o){//pass object
