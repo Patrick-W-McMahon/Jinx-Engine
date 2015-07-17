66bdb0af5f9237b69f27e88e0c15e26e5a186250
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2015-07-17 17:47:58 -0400

    Update menuUI.js
    cleaning up some commented code

--- examples/pong2/menuUI.js
+++ examples/pong2/menuUI.js
@@ -1,7 +1,5 @@
 function MenuUI(){
 	this.gameEngine;
-	//this.playerOneScore=0;
-	//this.playerTwoScore=0;
 	this.mousePos;
 	this.buttonHover=false;
 	this.buttionPos = {x:0,y:100,height:50,width:200}
@@ -43,23 +41,11 @@ function MenuUI(){
 	}
 	
 	this.input = function(keyDown,keyPress,KeyUp){
-		/*
-		if(keyDown.indexOf(19)>-1){//pause button
-			console.log("pause button");
-			this.gameEngine.addEvent({name:"GameEngine",message:"pause"});
-		}*/
+
 	}
 	
 	this.EventLisener = function(e){
-	/*
-		var pointEvent = this.gameEngine.getEventInStack("point",true);
-		if(pointEvent){
-			if(pointEvent.player==1){
-				this.playerOneScore++;
-			}else{
-				this.playerTwoScore++;
-			}
-		}*/
+
 	}
 	
 	this.DrawMouseCursor = function(g){
@@ -103,18 +89,6 @@ function MenuUI(){
 	}
 	
 	this.draw = function(g){
-	/*
-		g.fillStyle = "black";
-		g.font="24px Verdana";
-		if(this.gameEngine.frameCount<100){
-			g.fillText("Ready ",this.gameEngine.getDisplayWidth()/2,this.gameEngine.getDisplayHeight()/2);
-		}else if(this.gameEngine.frameCount>100&&this.gameEngine.frameCount<130){
-			g.fillText("GO!! ",this.gameEngine.getDisplayWidth()/2,this.gameEngine.getDisplayHeight()/2);
-		}else{
-			g.font="20px Verdana";
-			g.fillText(""+this.playerOneScore,10,18);
-			g.fillText(""+this.playerTwoScore,this.gameEngine.getDisplayWidth()-15,18);
-		}*/
 		g.font="34px Verdana";
 		g.fillStyle="black";
 		g.fillText("Pong Example Using Scenes",(this.gameEngine.getDisplayWidth()/2)-220,60);
