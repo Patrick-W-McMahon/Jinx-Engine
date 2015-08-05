70d6a07681860fdb32f201baa2f0ada3749d0866
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2015-08-05 14:53:00 -0400

    Create main.js

---
+++ examples/flappyBird/main.js
@@ -0,0 +1,29 @@
+var myDom = new DOM();
+var myEngine = new GameEngine();
+myEngine.engineMode="live";
+var gamePaused=0;
+myDom.OnReady(function(){
+	myEngine.setDisplay("screen");
+	myDom.getId("newGameButton").addEventListener("click", function(){
+		myEngine.stop();
+		var mainMenu = new Scene();
+		var menuUI = mainMenu.addObject(new MenuUI());
+		var cursor = mainMenu.addObject(new Cursor());
+		myEngine.setScene(mainMenu); 
+		myEngine.init();
+		myEngine.start();
+	}, false);
+	myDom.getId("PauseButton").addEventListener("click", function(){
+		if(gamePaused){
+			myEngine.start();
+			gamePaused=0;
+		}else{
+			myEngine.pause();
+			gamePaused=1;
+		}
+		
+	}, false);
+	myDom.getId("quitButton").addEventListener("click", function(){
+		myEngine.stop();
+	}, false);
+});
