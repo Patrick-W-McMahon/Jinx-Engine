aea1367c453ac9aaa08b65d78ba1cdef96c320bb
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2015-08-05 17:22:53 -0400

    Update player.js
    added in code to use an image to display the player.

--- examples/flappyBird/player.js
+++ examples/flappyBird/player.js
@@ -9,12 +9,18 @@ function Player(c,n,s){
 	this.gameEngine;
 	this.upKey;
 	this.movingUp=false;
+	this.img;
+	this.displayType="basic";//basic or image
 
 	
 	this.init = function(e){
 		this.gameEngine = e;
 		this.x = this.gameEngine.getDisplay("widthCenter");
 		this.y = this.gameEngine.getDisplay("heightCenter"); 
+		if(this.displayType=="image"){
+			this.img  = new Image(); 
+			this.img.src = "flappy_twitter.png";
+		}
 	}
 
 	this.input = function(keys){
@@ -48,12 +54,17 @@ function Player(c,n,s){
 	}
 	
 	this.draw = function(g){		
-		g.fillStyle = this.color;
-		g.beginPath();
-		g.arc(this.x,this.y,this.radius,0,2*Math.PI);
-		g.fill();
+		if(this.displayType=="image"){
+			g.drawImage(this.img,this.x-10,this.y-10,20,20);
+		}else{
+			g.fillStyle = this.color;
+			g.beginPath();
+			g.arc(this.x,this.y,this.radius,0,2*Math.PI);
+			g.fill();
+		}
 		g.fillStyle = "black";
 		g.font="12px Verdana";
 		g.fillText(""+this.name,this.x-20,this.y-10);
+		
 	}
 };
