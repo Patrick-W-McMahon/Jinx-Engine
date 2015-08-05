24592dad5fab03dd55c1432b4f0e2fa15bcc260b
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2015-08-05 14:53:31 -0400

    Create player.js

---
+++ examples/flappyBird/player.js
@@ -0,0 +1,56 @@
+function Player(c,n,s){
+	this.color=c;
+	this.score=0;
+	this.radius=8;
+	this.name=n;
+	this.x;
+	this.y=10;
+	this.speed=s;
+	this.gameEngine;
+	this.upKey;
+	this.movingUp=false;
+
+	
+	this.init = function(e){
+		this.gameEngine = e;
+		this.x = this.gameEngine.getDisplay("widthCenter");
+		this.y = this.gameEngine.getDisplay("heightCenter"); 
+	}
+
+	this.input = function(keys){
+		this.upKey=32;
+		if(keys[this.upKey]){//up key
+			this.y-=this.speed;
+			if(this.y<0){
+				this.y=0;
+			}
+			this.movingUp=true;
+		}else{
+			this.movingUp=false;
+		}
+	}
+		
+	this.update = function(){
+		var gameState = this.gameEngine.getEventInStack("gameover",false);
+		if(gameState==false){
+			if(!this.movingUp){
+				this.y+=this.speed;
+			}
+			if(this.y>this.gameEngine.getDisplayHeight()||this.y<0){
+				this.gameEngine.addEvent({name:"gameover",score:this.score});//Game Over Trigger
+			}
+			this.gameEngine.getEventInStack("player",true);
+			this.gameEngine.addEvent({name:"player",x:this.x,y:this.y,radius:this.radius});
+		}
+	}
+	
+	this.draw = function(g){		
+		g.fillStyle = this.color;
+		g.beginPath();
+		g.arc(this.x,this.y,this.radius,0,2*Math.PI);
+		g.fill();
+		g.fillStyle = "black";
+		g.font="12px Verdana";
+		g.fillText(""+this.name,this.x-20,this.y-10);
+	}
+};
